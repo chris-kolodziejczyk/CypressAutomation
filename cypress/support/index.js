1d48787112a4c9d7e2659dcb79e8a26edfa77ce1
Krzysztof Kołodziejczyk <krzysztof.kolodziejczyk21cn@gmail.com>
2020-09-29 15:52:47 +0200

    *

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,13 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+require('cypress-failed-log');
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+	console.log('err :' + err);
+	console.log('runnable :' + runnable);
+	return false;
+});
