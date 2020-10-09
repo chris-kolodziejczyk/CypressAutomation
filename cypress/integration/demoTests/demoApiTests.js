c9e6610c8873cff4dec7bf6c571b73f80f71b5da
Krzysztof Kołodziejczyk <krzysztof.kolodziejczyk21cn@gmail.com>
2020-10-09 16:29:30 +0200

    *

--- cypress/integration/demoTests/demoApiTests.js
+++ cypress/integration/demoTests/demoApiTests.js
@@ -3,12 +3,27 @@
 describe('Login page cookies test', () => {
 	beforeEach(function () {
 	
-		loginPage.visitPage();
+		
 	});
 
 	context('Login page cookies test', () => {
 		it('When accept Cookies Then checkCookiesNotVisible Should check if cookies warning is not visible', () => {
 			
+			cy.fixture('example').then((obj) => {
+
+				cy.request({
+				  url: obj.apiEndpoint,
+						  method: 'GET',
+						  headers: {},
+				  body: {},
+				  redirect: true
+				}).then((res) => {
+		  
+				  if(expect(res.status).to.be.equal(200)) {
+					console.log(res.body)
+				  }
+				})
+			  })
 		});
 	});
 });
--- cypress/integration/demoTests/demoTest.js
+++ cypress/integration/demoTests/demoTest.js
@@ -2,19 +2,19 @@
 
 describe('Cypress.io tests', function () {
   it.only('Open cypress.io page', function () {
-    var testArenaDemoPage = 'http://demo.testarena.pl/zaloguj';
+    let testArenaDemoPage = 'http://demo.testarena.pl/zaloguj';
     cy.visit(testArenaDemoPage);
     cy.url().should('include', 'demo.testarena');
     cy.get('#email').type('administrator@testarena.pl');
     cy.get('#password').type('sumXQQ72$L');
     cy.contains('Zaloguj').click();
 
-	cy.url().should('include', 'http://demo.testarena.pl');
-	
+    cy.url().should('include', 'http://demo.testarena.pl');
+
     // cy.url().then((url) => {
     //   assert.strictEqual(url, 'http://demo.testarena.pl/');
 
-	//   expect(url).to.equal('http://demo.testarena.pl/');
-	// });
+    //   expect(url).to.equal('http://demo.testarena.pl/');
+    // });
   });
 });
