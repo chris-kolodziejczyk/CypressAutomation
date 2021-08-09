b56df1f29cf0e32f6528def095b780da980e344e
Krzysztof <krzysztof.kolodziejczyk21cn@gmail.com>
2021-08-09 09:45:06 +0200

    Correction of the description

--- cypress/integration/demoTests/demoTest.js
+++ cypress/integration/demoTests/demoTest.js
@@ -1,21 +1,26 @@
 /// <reference types="cypress" />
 
 describe('Demo TestArena Tests', function () {
-  it.only('Demo login test', function () {
-    let testArenaDemoPage = 'http://demo.testarena.pl/zaloguj';
-    cy.visit(testArenaDemoPage);
-    cy.url().should('include', 'demo.testarena');
-    cy.get('#email').type('administrator@testarena.pl');
-    cy.get('#password').type('sumXQQ72$L');
-    cy.contains('Zaloguj').click();
 
-    cy.url().should('include', 'http://demo.testarena.pl');
+  context('Demo tests',() => {
+    it.only('Demo login test', function () {
+      let testArenaDemoPage = 'http://demo.testarena.pl/zaloguj';
+      cy.visit(testArenaDemoPage);
+      cy.url().should('include', 'demo.testarena');
+      cy.get('#email').type('administrator@testarena.pl');
+      cy.get('#password').type('sumXQQ72$L');
+      cy.contains('Zaloguj').click();
+  
+      cy.url().should('include', 'http://demo.testarena.pl');
+  
+      // NOTE: Przykłady innego wykorzystania asercji dla sprawdzenia adresu url
+      // cy.url().then((url) => {
+      //   assert.strictEqual(url, 'http://demo.testarena.pl/');
+  
+      //   expect(url).to.equal('http://demo.testarena.pl/');
+      // });
+  
+    });
+  })
 
-    // cy.url().then((url) => {
-    //   assert.strictEqual(url, 'http://demo.testarena.pl/');
-
-    //   expect(url).to.equal('http://demo.testarena.pl/');
-    // });
-
-  });
 });
