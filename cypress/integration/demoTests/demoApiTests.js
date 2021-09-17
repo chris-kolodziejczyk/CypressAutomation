397822aeb95417633059e7661aaa2fc4a6b14387
Krzysztof <krzysztof.kolodziejczyk21cn@gmail.com>
2021-09-17 14:37:38 +0200

    Minor project refactor update

--- cypress/integration/demoTests/demoApiTests.js
+++ cypress/integration/demoTests/demoApiTests.js
@@ -1,29 +1,23 @@
 /// <reference types="cypress" />
 
 describe('breweries list', () => {
-	beforeEach(() => {
-
-	});
+	beforeEach(() => {});
 
 	context('breweries list context', () => {
 		it('Log breweries list', () => {
-			
 			cy.fixture('example').then((obj) => {
-
 				cy.request({
-				  url: obj.apiEndpoint,
-						  method: 'GET',
-						  headers: {},
-				  body: {},
-				  redirect: true
-				})			
-				.then((res) => {
-		  
-					if(expect(res.status).to.be.equal(200)) {
-					  console.log(res.body)
+					url: obj.apiEndpoint,
+					method: 'GET',
+					headers: {},
+					body: {},
+					redirect: true,
+				}).then((res) => {
+					if (expect(res.status).to.be.equal(200)) {
+						console.log(res.body);
 					}
-				  })
-			  })
+				});
+			});
 		});
 	});
 });
--- cypress/integration/demoTests/demoTest.js
+++ cypress/integration/demoTests/demoTest.js
@@ -1,31 +1,23 @@
 /// <reference types="cypress" />
 
-import { LoginPage } from "../../pages/pageObjects/loginPage/loginPage";
-
 describe('Demo TestArena Tests', function () {
+	context('Demo tests', () => {
+		it.only('Demo login test', function () {
+			let testArenaDemoPage = 'http://demo.testarena.pl/zaloguj';
+			cy.visit(testArenaDemoPage);
+			cy.url().should('include', 'demo.testarena');
+			cy.get('#email').type('administrator@testarena.pl');
+			cy.get('#password').type('sumXQQ72$L');
+			cy.contains('Zaloguj').click();
 
-  context('Demo tests',() => {
-    it.only('Demo login test', function () {
-      
-    
-      
-      let testArenaDemoPage = 'http://demo.testarena.pl/zaloguj';
-      cy.visit(testArenaDemoPage);
-      cy.url().should('include', 'demo.testarena');
-      cy.get('#email').type('administrator@testarena.pl');
-      cy.get('#password').type('sumXQQ72$L');
-      cy.contains('Zaloguj').click();
-  
-      cy.url().should('include', 'http://demo.testarena.pl');
-  
-      // NOTE: Przykłady innego wykorzystania asercji dla sprawdzenia adresu url
-      // cy.url().then((url) => {
-      //   assert.strictEqual(url, 'http://demo.testarena.pl/');
-  
-      //   expect(url).to.equal('http://demo.testarena.pl/');
-      // });
-  
-    });
-  })
+			cy.url().should('include', 'http://demo.testarena.pl');
 
+			// NOTE: Przykłady innego wykorzystania asercji dla sprawdzenia adresu url
+			// cy.url().then((url) => {
+			//   assert.strictEqual(url, 'http://demo.testarena.pl/');
+
+			//   expect(url).to.equal('http://demo.testarena.pl/');
+			// });
+		});
+	});
 });
