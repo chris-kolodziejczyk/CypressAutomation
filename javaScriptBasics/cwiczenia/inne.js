fca5c75a5e8d23977ef6a13ffdb07fbf12fa3ac5
Krzysztof <krzysztof.kolodziejczyk21cn@gmail.com>
2021-10-12 09:52:09 +0200

    *

--- javaScriptBasics/cwiczenia/inne.js
+++ javaScriptBasics/cwiczenia/inne.js
@@ -1,12 +1,15 @@
-// NOTE: Napisz program JavaScript, aby sprawdzić dwie podane liczby i zwróć true, jeśli jedna z liczb to 50 lub jeśli ich suma to 50
+// NOTE: Napisz program JavaScript, aby sprawdzić dwie podane liczby i zwróć true, jeśli jedna z liczb to 50
+//          lub jeśli ich suma to 50
 
 // *****************************************************************
 
-// NOTE: Napisz program JavaScript, aby sprawdzić z dwóch podanych liczb całkowitych, czy jedna jest dodatnia, a druga ujemna
+// NOTE: Napisz program JavaScript, aby sprawdzić z dwóch podanych liczb całkowitych, czy jedna jest dodatnia,
+//          a druga ujemna
 
 // *****************************************************************
 
-// NOTE: Napisz program JavaScript, aby utworzyć nowy ciąg, dodając „JS” przed danym ciągiem. Jeśli podany ciąg zaczyna się od „JS”, zwróć oryginalny ciąg
+// NOTE: Napisz program JavaScript, aby utworzyć nowy ciąg, dodając „JS” przed danym ciągiem.
+//          Jeśli podany ciąg zaczyna się od „JS”, zwróć oryginalny ciąg
 
 // *****************************************************************
 
