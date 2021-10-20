92556a0d581758b7bd424662d2a8abb18c3c058a
Krzysztof <krzysztof.kolodziejczyk21cn@gmail.com>
2021-10-20 11:31:08 +0200

    Add task demo test

--- cypress/integration/demoTests/tasksTests.js
+++ cypress/integration/demoTests/tasksTests.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 import { mainPage } from '../../pages/pageObjects/mainPage/mainPage';
 import { loginPage } from '../../pages/pageObjects/loginPage/loginPage';
 import { tasksPage } from '../../pages/pageObjects/tasksPage/tasksPage';
@@ -7,6 +9,20 @@ describe('Demo TestArena Tests', function () {
 		it.only('Demo task page test', function () {
 			loginPage.visitPage().login();
 			mainPage.goToMenuOpt('Zadania');
+			tasksPage
+				.fillTaskForm(
+					tasksPage.randomString(10, 'letters'),
+					tasksPage.randomString(10, 'letters'),
+					tasksPage.randomString(10, 'letters'),
+					'2',
+					1,
+					'2',
+					1,
+					'Krytyczny',
+					'2020-06-06 23:59',
+					'me'
+				)
+				.checkAddTaskConfirmation();
 		});
 	});
 });
