4eee4ed83b04c03c1cfd724409543a4979813ada
Krzysztof <krzysztof.kolodziejczyk21cn@gmail.com>
2021-11-23 09:27:23 +0100

    *

--- cypress/pages/pageObjects/mainPage/mainPageSelectors.js
+++
@@ -1,4 +0,0 @@
-export default {
-	logOutBtn: '#head-top > div.top_right > div.header_logout > a > span',
-	menuList: '#wrapper > ul',
-};
--- cypress/pages/pageObjects/testBase/testBaseSelectors.js
+++
@@ -1,9 +0,0 @@
-export default {
-	addTests: '#content > article > nav > ul > div > a',
-	addTestsList: '#content > article > nav > ul > div > div > ul',
-	testName: '#name',
-	testDescription: '#description',
-	testPresuppositions: '#presuppositions',
-	testResult: '#result',
-	saveTest: '#add',
-};
