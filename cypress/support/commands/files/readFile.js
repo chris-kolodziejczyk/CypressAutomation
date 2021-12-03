642b8d2a731774371c6b50f2da3d443fa2b70cf6
Krzysztof <krzysztof.kolodziejczyk21cn@gmail.com>
2021-12-03 10:16:00 +0100

    *

---
+++ cypress/support/commands/files/readFile.js
@@ -0,0 +1,9 @@
+Cypress.Commands.add('readDataFromFile', (filePath, encoding) => {
+
+	cy.get(loginPageSelectors.email).type(email);
+	cy.get(loginPageSelectors.pass).type(pass);
+	cy.contains(loginBtnText).click();
+
+		 
+
+});
