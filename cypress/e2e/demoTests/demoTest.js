1ccc9c8ec1880824a65c552883a6c5640c4b2a0a
testerspl <110538354+testerspl@users.noreply.github.com>
2023-01-20 10:34:03 +0100

    *

--- cypress/e2e/demoTests/demoTest.js
+++ cypress/e2e/demoTests/demoTest.js
@@ -1,83 +1,20 @@
 /// <reference types="cypress" />
 
-import { loginPage } from '../../pages/pageObjects/loginPage/loginPage';
-import { mainPage } from '../../pages/pageObjects/mainPage/mainPage';
-import { testBasePage } from '../../pages/pageObjects/testBasePage/testBasePage';
-
 describe('Demo TestArena Tests', function () {
-	it('Demo login test', function () {
-		loginPage.visitPage();
-		loginPage.login();
-		loginPage.checkLogin();
-	});
+	it.only('Demo login test', function () {
+		let testArenaDemoPage = 'http://demo.testarena.pl/zaloguj';
+		cy.visit(testArenaDemoPage);
+		cy.url().should('include', 'demo.testarena');
+		cy.get('#email').type('administrator@testarena.pl');
+		cy.get('#password').type('sumXQQ72$L');
+		cy.contains('Zaloguj').click();
 
-	it('Demo logout test', function () {
-		loginPage.visitPage();
-		loginPage.login();
-		mainPage.logOut();
+		cy.url().should('include', 'http://demo.testarena.pl');
 
-		cy.url().should('include', '/zaloguj');
-	});
+		cy.url().then((url) => {
+			assert.strictEqual(url, 'http://demo.testarena.pl/');
 
-	it('Demo add test to test base', function () {
-		loginPage.visitPage();
-		loginPage.login();
-		mainPage.goToTestBase();
-		testBasePage.addTestToTestBase(
-			1,
-			'Przypadek testowy',
-			testBasePage.randomString(200),
-			testBasePage.randomString(300),
-			testBasePage.randomString(500)
-		);
-		testBasePage.checkAddedTest();
-	});
-
-	it('Demo add test with to long data', function () {
-		loginPage.visitPage();
-		loginPage.login();
-		mainPage.goToTestBase();
-		testBasePage.addTestToTestBase(
-			1,
-			'Przypadek testowy',
-			testBasePage.randomString(5000),
-			'dvgrgrg',
-			'gergergre'
-		);
-		testBasePage.checkAddedTest();
-	});
-
-	it('Demo add test with short string', function () {
-		loginPage.visitPage();
-		loginPage.login();
-		mainPage.goToTestBase();
-		testBasePage.addTestToTestBase(
-			1,
-			'Przypadek testowy',
-			testBasePage.randomString(2),
-			testBasePage.randomString(555),
-			testBasePage.randomString(2)
-		);
-		testBasePage.checkAddedTest();
-	});
-
-	it('Demo add test with short string', function () {
-		loginPage.visitPage();
-		loginPage.login();
-		mainPage.goToTestBase();
-		testBasePage.searchTest().checkTestOnList();
-	});
-
-	it.only('Demo false login test', function () {
-		cy.fixture('example/example').then((obj) => {
-			loginPage.visitPage();
-			loginPage.login(obj.falseEmail, obj.falsePass);
-			cy.get(
-				'div.formContainer:nth-child(2) > span:nth-child(1) > div:nth-child(3)'
-			).should(
-				'have.text',
-				'Nieprawidłowy format adresu e-mail. Wprowadź adres ponownie.'
-			);
+			expect(url).to.equal('http://demo.testarena.pl/');
 		});
 	});
 });
